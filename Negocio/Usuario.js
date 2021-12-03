5fa5e4ada660b27f60cd5e09d7678bfe1427f6a0
andressa-oliveira21051 <95000057+andressa-oliveira21051@users.noreply.github.com>
2021-12-03 15:02:16 -0300

    Update Usuario.js

---
+++ Negocio/Usuario.js
@@ -0,0 +1,13 @@
+class Usuario{
+    constructor(id,nome, email, senha, foto, interfaceTema, interfaceFonte){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.foto = foto;
+        this.interfaceTema = interfaceTema;
+        this.interfaceFonte = interfaceFonte;
+    }
+}
+
+export default Usuario; 
