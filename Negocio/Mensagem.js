dced108f023c71a66e372847376e36c376408fc8
Salieri19168 <95000139+Salieri19168@users.noreply.github.com>
2021-12-03 14:58:47 -0300

    Update Mensagem.js

--- Negocio/Mensagem.js
+++ Negocio/Mensagem.js
@@ -1,2 +1,14 @@
+class Mensagem{
+    constructor(texto,data,usuario,pemiteComentario,corFundo,CorFonte,imagem){
+        this.texto = texto;
+        this.data = data;
+        this.usuario = usuario;
+        this.pemiteComentario = pemiteComentario;
+        this.corFundo = corFundo;
+        this.CorFonte = CorFonte;
+        this.imagem = imagem;
+    }
+}
 
+export default Mensagem;
 
