c827c83554a599259ae85e76646ca2f2db57f87f
tomast1337 <vycasnicolas@gmail.com>
2021-12-04 00:21:22 -0300

    Adicionado feed

---
+++ pages/feed.js
@@ -0,0 +1,22 @@
+
+
+class Feed extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Feed</h1>
+                <p>This is the feed page</p>
+            </div>
+        );
+    }
+
+}
+
+export default Feed;
