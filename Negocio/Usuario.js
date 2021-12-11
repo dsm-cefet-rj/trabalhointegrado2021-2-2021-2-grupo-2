9084a1839b47e43506041ce491a828dd9d346e2e
tomast1337 <vycasnicolas@gmail.com>
2021-12-10 22:09:26 -0300

    Alterado Negocio Usuario

--- Negocio/Usuario.js
+++ Negocio/Usuario.js
@@ -1,13 +1,21 @@
-class Usuario{
-    constructor(id,nome, email, senha, foto, interfaceTema, interfaceFonte){
+import Configuracao from "./Configuracao";
+
+class Usuario {
+    constructor(id, nome, email, senha, foto, moderador, mensagens, propagandas,
+        grupos, temaInterface, fonteInterface) {
+
         this.id = id;
         this.nome = nome;
         this.email = email;
         this.senha = senha;
         this.foto = foto;
-        this.interfaceTema = interfaceTema;
-        this.interfaceFonte = interfaceFonte;
+        this.moderador = moderador;
+        this.mensagens = mensagens;
+        this.propagandas = propagandas;
+        this.grupos = grupos;
+        this.temaInterface = temaInterface;
+        this.fonteInterface = fonteInterface;
     }
 }
 
-export default Usuario; 
+export default Usuario;
