d6c8f23a086da01f48a15b37bcef20fbe245659c
tomast1337 <vycasnicolas@gmail.com>
2021-12-10 22:09:39 -0300

    Adicionado Negocio Propaganda

---
+++ Negocio/Propaganda.js
@@ -0,0 +1,14 @@
+class Propaganda{
+    constructor(id,nome,imagem,usuarioDono,link,dataCriacao,visualizacoes){
+        this.id = id;
+        this.nome = nome;
+        this.imagem = imagem;
+        this.usuarioDono = usuarioDono;
+        this.link = link;
+        this.dataCriacao = dataCriacao;
+        this.visualizacoes = visualizacoes;
+
+    }   
+}
+
+export default Propaganda;
