4f2f6ba93fd28c6e78aceb9771fcd7f1f695fb0e
tomast1337 <vycasnicolas@gmail.com>
2021-12-13 14:51:04 -0300

    Modificado page/index

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,69 @@
-import React from "react";
-import Login from "../components/ContaLogin";
+import React, { useEffect } from "react";
+import { useSession, signIn } from "next-auth/react"
+import { Button } from "react-bootstrap";
+import DefaultLayout from "../components/DefaultLayout";
+import Mensagem from "../components/Mensagem";
+import style from "../styles/index.module.scss";
 
 
-class Index extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            nome: "",
-            email: "",
-            senha: "",
-            confirmarSenha: "",
-            error: '',
-            tipo: 'login',
-        };
-    }
+export default function Index() {
+    const { data: session } = useSession()
+    const [messagesData, setMessagesData] = React.useState(null);
+    const [error, setError] = React.useState(null);
 
-    changePage = (e) => {
-        if (this.state.tipo === 'login') {
-            this.setState({
-                tipo: 'cadastro'
-            })
-        } else {
-            this.setState({
-                tipo: 'login'
-            })
+    useEffect(async () => {
+        if (session) {
+            const response = await fetch("api/feed", {
+                method: "GET",
+            }).then(res => {
+                if (res.ok) {
+                    return res.json()
+                }
+                throw new Error("Não foi possivel obter as mensagens")
+            }).then(data => {
+                setMessagesData(data)
+            }).catch(err => {
+                setError(err)
+            }, [])
         }
-    }
-    render() {
+    });
 
-        return (
-            <div className="loginPage">
-                <div className="loginHeader">
-                    <h1>Djan</h1>
-                </div>
-                <h2>Login</h2>
-                <Login/>
-            </div>
-        );
-
-    }
+    return (
+        <>
+            {
+                session ? (
+                    <DefaultLayout>
+                        <div className={style.feedContainer}>
+                            <h1>Feed</h1>
+                            {error && <p>{error.message}</p>}
+                            {messagesData ?
+                                messagesData.map(message => {
+                                    return (
+                                        <Mensagem key={message._id} Mensagem={message} />
+                                    )
+                                }) : (
+                                    <div className={style.finalMessage}>
+                                        <h2>Carregando</h2>
+                                    </div>
+                                )}
+                            <div className={style.finalMessage}>
+                                <h2>Fim das mensagens</h2>
+                            </div>
+                        </div>
+                    </DefaultLayout>
+                ) : (
+                    <div className={style.loginPage} >
+                        <div className={style.loginHeader}>
+                            <h1>Djan</h1>
+                        </div>
+                        <div>
+                            <div className="row">
+                                <Button onClick={() => signIn('github')} variant="outline-success btn-lg">Login com GitHub</Button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </>
+    );
 }
-
-export default Index;
